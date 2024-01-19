8442bb58c2ffe5a2d719016dd266fc57139b3ae7
Monesh Goyal <goyalmonesh2201@gmail.com>
2024-01-20 01:29:32 +0530

    test changes

--- app/components/CanvasBoard.jsx
+++ app/components/CanvasBoard.jsx
@@ -6,26 +6,26 @@ import { useContext } from 'react';
 import ShapeContext from '@/context/ShapeContext';
 
 const CanvasBoard = () => {
-  const canvasRef = useRef(null);
-  const fabricCanvasRef = useRef(null); // this line can be deleted
+  // - const canvasRef = useRef(null);
+  // - const fabricCanvasRef = useRef(null); // this line can be deleted
 
   // context related
 
-  const {rect} = useContext(ShapeContext);
+  // - const {rect} = useContext(ShapeContext);
 
-  useEffect(() => {
-    const canvas = new fabric.Canvas(canvasRef.current);
-    fabricCanvasRef.current = canvas; // this line can be deleted
-    const recta = new fabric.Rect({
-     left: 100,
-     top: 100,
-     width: 50,
-     height: 50,
-     fill: 'red',
-    });
+  //- useEffect(() => {
+  //-   const canvas = new fabric.Canvas(canvasRef.current);
+  //-   fabricCanvasRef.current = canvas; // this line can be deleted
+  //-   const recta = new fabric.Rect({
+  //-    left: 100,
+  //-    top: 100,
+  //-    width: 50,
+  //-    height: 50,
+  //-    fill: 'red',
+  //-   });
 
-    canvas.add(recta);
-  }, [rect])
+  //-   canvas.add(recta);
+  //- }, [rect])
   
   // useEffect(() => {
   //   const canvas = new fabric.Canvas(canvasRef.current);
@@ -77,12 +77,51 @@ const CanvasBoard = () => {
 
   // }, []);
 
+
+  const canvasRef = useRef(null);
+  const canvas = useRef(null);  
+
+  // context 
+  const {rect} = useContext(ShapeContext);
+
+  useEffect(() => {
+    if(canvas.current){
+      const rect = new fabric.Rect({
+       left: 100,
+       top: 100,
+       width: 50,
+       height: 50,
+       fill: 'red',
+      });  
+      canvas.current.add(rect);  
+    }
+  }, [rect])  
+
+  useEffect(() => {
+    canvas.current = new fabric.Canvas(canvasRef.current, {
+      width: 400,
+      height: 550
+    })
+
+    canvas.current.on('mouse:down', (options) => {        // This function is optional can be discarded in future
+      if (options.target === null) {
+        canvas.current.discardActiveObject().renderAll();
+      }
+    });    
+
+    return () => {
+      if (canvas.current) {
+        canvas.current.dispose();
+      }
+    };
+  },[])
+
   return (
     
       <canvas
       ref={canvasRef}
-      width={400}
-      height={550}
+      //- width={400}
+      //- height={550}
       className='border'
     />
 
